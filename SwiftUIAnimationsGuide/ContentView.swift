284435ddbefbe5c7efe521b50cdb094d8c1f1613
Abdul Karim Khan <abdulkarimkhan.cs@hotmail.com>
2023-07-29 21:32:04 +0500

    Initial Commit

---
+++ SwiftUIAnimationsGuide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimationsGuide
+//
+//  Created by Abdul Karim Khan on 29/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAnimationsGuide/SwiftUIAnimationsGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsGuideApp.swift
+//  SwiftUIAnimationsGuide
+//
+//  Created by Abdul Karim Khan on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
